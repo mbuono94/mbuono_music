235f92dd53c5e58c933be6f773559cbc9ec4322d
Michael Buono <michaelfbuono94@gmail.com>
2016-07-23 14:11:03 -0400

    Added new pictures, works on mobile now

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $message = $_POST['message'];
+    $from = 'From: Michael Buono Music'; 
+    $to = 'michaelfbuono94@gmail.com'; 
+    $subject = 'Contact Michael email';
+    $human = $_POST['human'];
+			
+    $body = "From: $name\n E-Mail: $email\n Message:\n $message";
+				
+    if ($_POST['submit'] && $human == '4') {				 
+        if (mail ($to, $subject, $body, $from)) { 
+	    echo '<p>Your message has been sent!</p>';
+	} else { 
+	    echo '<p>Something went wrong, go back and try again!</p>'; 
+	} 
+    } else if ($_POST['submit'] && $human != '4') {
+	echo '<p>You answered the anti-spam question incorrectly!</p>';
+    }
+?>
