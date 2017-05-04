7407ae4c781ea1722d50252fa4f13041fc24325d
Michael Buono <michaelfbuono94@gmail.com>
2017-05-03 19:43:11 -0700

    HUGE CHANGE: went to one page site with me sitting on top of the railing

---
+++ old website/button.js
@@ -0,0 +1,13 @@
+var message = "";
+
+$("#sendMessage").on("click", function() {
+    message = $("#contactform").serialize();
+    $.ajax({
+        url: "https://formspree.io/michaelfbuono94@gmail.com", 
+        method: "POST",
+        data: {message: message},
+        dataType: "json"
+    });
+    alert('Thanks for reaching out! I will get back to you as soon as possible.');
+    return false;
+});
